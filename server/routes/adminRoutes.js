51cf71ec82d9df69453b25243980d8c87b3d0eb8
milkias <milkiastolesa53@gmail.com>
2026-07-07 08:21:22 +0300

    backend

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { getDashboardStats, getAllUsers, deleteUser } = require('../controllers/adminController');
+const {
+  getDashboardStats,
+  getAllUsers,
+  deleteUser,
+  exportUsersToExcel,
+  exportBookingsToExcel
+} = require('../controllers/adminController');
 const protect = require('../middleware/auth');
 const admin = require('../middleware/admin');
 
@@ -9,5 +15,9 @@ router.use(protect, admin);
 router.get('/stats', getDashboardStats);
 router.get('/users', getAllUsers);
 router.delete('/users/:id', deleteUser);
+
+// Excel Export Routes
+router.get('/users/export/excel', exportUsersToExcel);
+router.get('/bookings/export/excel', exportBookingsToExcel);
 
 module.exports = router;
