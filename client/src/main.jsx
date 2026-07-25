62745c52621031c8393ee455146aa0fe1d651402
milkias <milkiastolesa53@gmail.com>
2026-07-25 16:04:38 +0300

    vercel web analytics

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,6 +5,8 @@ import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext'
 import { Toaster } from 'react-hot-toast'
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -12,6 +14,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <AuthProvider>
         <App />
         <Toaster position="top-right" toastOptions={{ duration: 3500 }} />
+        <Analytics />
+        <SpeedInsights />
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>,
